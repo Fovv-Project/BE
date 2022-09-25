2422c11efb13e8081c3794dd6350a937f87be11a
Raihan <raihanalmenata@gmail.com>
2022-09-25 11:42:45 +0700

    Express installation and basic configuration.

--- src/app/app.js
+++ src/app/app.js
@@ -1,2 +1,12 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
 // Configure dotenv
-require('dotenv').config()
+dotenv.config()
+
+// Initiates app
+const app = express()
+const port = process.env.port || 3000
+app.listen(port, ()=> {
+    console.log(`App is listening on port ${port}`);
+})
