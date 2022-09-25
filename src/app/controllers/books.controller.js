d6c8cfbee36ec0530af3e8f3758749eea176ce07
Raihan <raihanalmenata@gmail.com>
2022-09-25 16:59:40 +0700

    Added controllers templates.

---
+++ src/app/controllers/books.controller.js
@@ -0,0 +1,17 @@
+module.exports = {
+    get : async (req, res, next) => {
+
+    },
+
+    getId : async (req, res, next) => {
+
+    },
+
+    insert : async (req, res, next) => {
+
+    },
+
+    remove : async (req, res, next) => {
+
+    },
+}
---
+++ src/app/controllers/borrows.controller.js
@@ -0,0 +1,29 @@
+module.exports = {
+    get : async (req, res, next) => {
+
+    },
+
+    getId : async (req, res, next) => {
+
+    },
+
+    updateId : async (req, res, next) => {
+
+    },
+
+    updateIdApproval : async (req, res, next) => {
+
+    },
+
+    updateIdStatus : async (req, res, next) => {
+
+    },
+
+    insert : async (req, res, next) => {
+
+    },
+
+    removeId : async (req, res, next) => {
+
+    },
+}
