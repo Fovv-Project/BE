a3164e0b23de58365192e83bb55e15e4c76d966b
Raihan <raihanalmenata@gmail.com>
2022-09-26 23:08:16 +0700

    Routes assigned automatically.

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,11 @@
-const express = require('express')
 const dotenv = require('dotenv')
+const { getRoutedApp } = require('./services/app.service')
 
 // Configure dotenv
 dotenv.config()
 
 // Initiates app
-const app = express()
+const app = getRoutedApp()
 const port = process.env.port || 3000
 app.listen(port, ()=> {
     console.log(`App is listening on port ${port}`);
