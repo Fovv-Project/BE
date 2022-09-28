880d8be78f3a0ea14908e836d7e2b266519610eb
Raihan <raihanalmenata@gmail.com>
2022-09-28 10:19:45 +0700

    Assigned logger middleware.

--- src/app/routes/attendances.route.js
+++ src/app/routes/attendances.route.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+/* Middleware */
+router.use(require('../middlewares/logger.middleware'))
+
+/* Route */
 router.get('/')
 
 module.exports = router
--- src/app/routes/borrows.route.js
+++ src/app/routes/borrows.route.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+/* Middleware */
+router.use(require('../middlewares/logger.middleware'))
+
+/* Routes */
 router.get('/')
 router.get('/:id')
 router.post('/')
