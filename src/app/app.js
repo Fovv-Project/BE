e554845655f53d2aa5d001ea02cc0db5f360fd5d
Raihan <raihanalmenata@gmail.com>
2022-10-07 09:23:44 +0700

    Renewed app.util import

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv')
-const { getRoutedApp } = require('./services/app.service')
+const { getRoutedApp } = require('../utils/app.util')
 
 // Configure dotenv
 dotenv.config()
