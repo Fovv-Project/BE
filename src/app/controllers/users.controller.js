52398853d1049576597c9aff9a19e61f25c5dc9d
Nurul Akhni <nurulakhni12@gmail.com>
2022-10-13 13:34:28 +0700

    added function implementation user

--- src/app/controllers/users.controller.js
+++ src/app/controllers/users.controller.js
@@ -1,8 +1,35 @@
+import User from "../models/users.model.js";
+
 module.exports = {
-    get : async (req, res, next) => {
+    get: async(req, res, next) => {
+        try {
+            const response = await User.findAll();
+            res.status(200).json({
+                success: true,
+                code: 200,
+                message: "Get User record successfully",
+                data: response
+            });
+        } catch (error) {
+            return res.status(500).send(error.message);
 
+        }
     },
-    getId : async (req, res, next) => {
-
+    getNim: async(req, res, next) => {
+        try {
+            const response = await User.findOne({
+                where: {
+                    nim: req.params.nim
+                }
+            });
+            res.status(200).json({
+                success: true,
+                code: 200,
+                message: `Get User with nim ${req.params.nim} successfully`,
+                data: response
+            });
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
     },
 }
