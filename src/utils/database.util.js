8e0cef2e7c2c9eba50336d31dda53e1b109d8aa9
Nilam Musdalifa <nilammusdalifa09@gmail.com>
2022-10-25 13:37:15 +0700

    Rename file database to database.util

---
+++ src/utils/database.util.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config()
+
+const db = new Sequelize(`${process.env.POSTGRES_DATABASE}`, `${process.env.POSTGRES_USER}`, `${process.env.POSTGRES_PASSWORD}`, {
+    host: `${process.env.POSTGRES_HOST}`,
+    dialect: `${process.env.POSTGRES_CONNECTION}`
+});
+
+module.exports = db;
