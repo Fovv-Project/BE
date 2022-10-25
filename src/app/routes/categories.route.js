2b0848b507d7a2f9033913e7d96ef723e680212f
Nurul Akhni <nurulakhni12@gmail.com>
2022-10-25 15:27:07 +0700

    update: call category controller func

--- src/app/routes/categories.route.js
+++ src/app/routes/categories.route.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const Category = require('../controllers/categories.controller')
+const categoryController = require('../controllers/categories.controller')
 
 /* Middleware */
 router.use(require('../middlewares/logger.middleware'))
 
 /* Route */
-router.get('/', Category.get)
+router.get('/', categoryController.get)
 router.post('/', [
     require('../middlewares/auth.middleware'),
-])
+], categoryController.insert)
 router.patch('/:id', [
     require('../middlewares/auth.middleware'),
-])
+], categoryController.updateId)
 router.delete('/:id', [
     require('../middlewares/auth.middleware'),
-])
+], categoryController.removeId)
 
 module.exports = router
