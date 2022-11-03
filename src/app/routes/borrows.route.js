5664e00a04c411ff3325167a07457b074b56cef3
Nurul Akhni <nurulakhni12@gmail.com>
2022-11-03 13:53:04 +0700

    update: define controller funct

--- src/app/routes/borrows.route.js
+++ src/app/routes/borrows.route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const borrowController = require('../controllers/borrows.controller')
 
 /* Middleware */
 router.use(require('../middlewares/logger.middleware'))
@@ -7,15 +8,15 @@ router.use(require('../middlewares/logger.middleware'))
 /* Routes */
 router.get('/', [
     require('../middlewares/auth.middleware'),
-])
+], borrowController.get)
 router.get('/:id', [
     require('../middlewares/auth.middleware'),
-])
+], borrowController.getId)
 router.post('/', [
     require('../middlewares/auth.middleware'),
-])
+], borrowController.insert)
 router.delete('/:id', [
     require('../middlewares/auth.middleware'),
-])
+], borrowController.removeId)
 
 module.exports = router
