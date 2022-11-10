d2a1aaec3513052b5b6ad52254037791b29b4e15
Raihan <raihanalmenata@gmail.com>
2022-11-10 13:15:36 +0700

    Added get all and get by nim handler at attendances controller

--- src/app/controllers/attendances.controller.js
+++ src/app/controllers/attendances.controller.js
@@ -1,13 +1,49 @@
+const db = require('../../utils/db.setup.util')
+const { ForbiddenResourceError, UnauthorizedError } = require('../../errors/utils/errors.interface.util')
+const { attendance } = db.models
+
 module.exports = {
-    get: async(req, res, next) => {
+    get: async (req, res, next) => {
+        try {
+
+            if (res.locals.userInfo.admin == false)
+                throw new ForbiddenResourceError("Forbidden resource")
+                
+            return res.status(200).json({
+                success: true,
+                code: 200,
+                message: "Get attendances record successfully",
+                data: await attendance.findAll()
+            });
+
+        } catch (err) {
+            next(err)
+        }
+    },
+
+    getId: async (req, res, next) => {
+        
+        try {
+
+            const userNim = res.locals.userInfo.nim
+            return res.status(200).json({
+                success: true,
+                code: 200,
+                message: "Get attendances record successfully",
+                data: await attendance.findOne({
+                    where: {
+                        nim: userNim
+                    }
+                })
+            });
+            
+        } catch (err) {
+            next(err)
+        }
 
     },
 
-    getId: async(req, res, next) => {
-
-    },
-
-    insert: async(req, res, next) => {
+    insert: async (req, res, next) => {
 
     },
 }
