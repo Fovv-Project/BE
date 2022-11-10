3791e27e1b54f5bbf3d48bf14f82b9bb603cbad5
Raihan <raihanalmenata@gmail.com>
2022-11-10 13:16:12 +0700

    Updated attendances routes

--- src/app/routes/attendances.route.js
+++ src/app/routes/attendances.route.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const attendanceController = require('../controllers/attendances.controller')
 
 /* Middleware */
 router.use(require('../middlewares/auth.middleware'))
 router.use(require('../middlewares/logger.middleware'))
 
 /* Route */
-router.get('/')
-router.get('/:nim')
+router.get('/', attendanceController.get)
+router.get('/:nim', attendanceController.getId)
 router.post('/')
 
 module.exports = router
