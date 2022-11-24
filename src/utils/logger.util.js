1ae86ca1009e88157cce0e2ee7ce46aa3f67afd7
Nurul Akhni <nurulakhni12@gmail.com>
2022-11-24 10:37:29 +0700

    update: change level logger to uppercase

--- src/utils/logger.util.js
+++ src/utils/logger.util.js
@@ -3,7 +3,7 @@ const { createLogger, format, transports } = require('winston');
 const customFormat = format.combine(
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     format.printf(
-        (info) => `[${info.timestamp}] [${info.level}] ${info.message}`
+        (info) => `[${info.timestamp}] ${info.level.toUpperCase()}! ${info.message}`
     )
 );
 
