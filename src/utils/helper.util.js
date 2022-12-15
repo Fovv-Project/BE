ae7bc69dc8af441b29a9a1e2b429c3be01630dbc
Raihan <raihanalmenata@gmail.com>
2022-12-15 07:09:10 +0700

    Added isInteger and rangedRandomInt utilities

--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -1,9 +1,36 @@
+const crypto = require('crypto')
+
 module.exports = {
-    isNumeric : (text) => {
-        [...text].forEach(letter => {
-          if (isNaN(letter))
-            return false
-        });
-        return true
+
+  isNumeric: (text) => {
+    for (letter of text) {
+      if (isNaN(letter)) {
+        return false
       }
+    }
+    return true
+  },
+
+  getRandom: (type, total) => {
+
+    let characters
+    if (type == "letter")
+      characters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+    else if (type == "number")
+      characters = "0123456789"
+    else
+      characters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ" + "0123456789"
+
+    let result = ""
+    for (let index = 0; index < total; index++) {
+      result += characters[crypto.randomInt(0, characters.length - 1)]
+    }
+    return result
+    
+  },
+
+  isInteger: (string) => isNaN(string) ? false : (Number.isInteger(parseFloat(string)) ? true : false),
+
+  rangedRandomInt : (min, max) => crypto.randomInt(0, max + 1)
+
 }
