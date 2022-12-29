d13e666a8910a85a1c0ce34ad26055cccae320c2
Raihan <raihanalmenata@gmail.com>
2022-12-29 14:33:49 +0700

    Added cron scheduler setup

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,16 @@
 const { getRoutedApp } = require('../utils/app.util')
+const cron = require('../utils/cron.util')
+const logger = require('../utils/logger.util')
 
 // Initiates app
 const app = getRoutedApp()
 const port = process.env.port || 3000
 app.listen(port, ()=> {
     console.log(`App is listening on port ${port}`);
+    try {
+        // Cron Jobs
+        cron.approvalRejector.start()  
+    } catch (err) {
+        logger.error('error-handler', err.message)
+    }
 })
