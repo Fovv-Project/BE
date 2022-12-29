98ed8667a4ba23df80fa6434af5eb5d42c688994
Raihan <raihanalmenata@gmail.com>
2022-12-29 14:50:14 +0700

    Added page not found handler

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,9 @@
 const { getRoutedApp } = require('../utils/app.util')
+const PageNotFoundHandler = require('./middlewares/pageNotFound.middleware')
 
 // Initiates app
 const app = getRoutedApp()
+app.use(PageNotFoundHandler)
 const port = process.env.port || 3000
 app.listen(port, ()=> {
     console.log(`App is listening on port ${port}`);
